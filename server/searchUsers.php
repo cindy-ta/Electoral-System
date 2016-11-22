aa58fbb5c0d445afed5cfed25ec9ac49e7ff2f95
cindy-ta <cindy-ta@uiowa.edu>
2016-11-22 14:41:01 -0600

    Fixing tony

--- server/searchUsers.php
+++ server/searchUsers.php
@@ -3,19 +3,21 @@ require_once '../includes/db.php'; // The mysql database connection script
 //if(isset($_GET['user_name']) && isset($_GET['user_type']))
 //{
     $username = $_GET['user_name'];
-    $user_type = $_GET['user_type'];
-    $first_name = $_GET('first_name'];
-    $last_name = $_GET('last_name'];
-    $government_id = $_GET('government_id'];
-    $zip_code = $_GET('zip_code'];
-
-
     $success = 1;
+    $user_type = $_GET['user_type'];
+    $first_name = $_GET['first_name'];
+    $last_name = $_GET['last_name'];
+    $government_id = $_GET['government_id'];
+    $zip_code = $_GET['zip_code'];
+
+
+
 
     if ($user_type == "Voter"){
         $query="SELECT * FROM Voters WHERE voter_id ='$username'";
         $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
-    }else{ //if($user_type == false){
+    }
+    else{ //if($user_type == false){
         $query="SELECT * FROM Managers WHERE manager_id ='$username'";
         $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
     }
