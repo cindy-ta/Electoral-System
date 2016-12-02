f2c07685c5fdbd9852f6690f6282a5ee38b7c0b2
cindy-ta <cindy-ta@uiowa.edu>
2016-12-02 15:21:15 -0600

    PDF information filled with election title and candidate name

--- app/ballotCtrl.js
+++ app/ballotCtrl.js
@@ -306,13 +306,15 @@ app.controller("ballotCtrl", function(md5, $http, $scope, $rootScope, uuid2, $lo
             {
                 $scope.message = "Vote successfully saved!!"
                 checkIfVoted();
-                var doc = new jsPDF();
-                doc.text('Voter ID: '+$rootScope.session.user_name+'\n\n'+'Election ID: '+$scope.selectedElection[0].election_id+'\n\n'+'Candidate ID: '+ $scope.selected_candidate, 10, 10);
-                doc.save('a4.pdf');
+                $http.post('server/getCandidate.php?candidate_id=' + $scope.selected_candidate).success(function (candidate) {
+                    var doc = new jsPDF();
+                    doc.text('Voter ID: '+$rootScope.session.user_name+'\n\n'+'Election ID: '+$scope.selectedElection[0].election_id+'\n\n'+'Candidate ID: '+ candidate.first_name + ' ' + candidate.last_name, 10, 10);
+                    doc.save('a4.pdf');
+                })
+
             }
 
         })
-
 
     }
 
