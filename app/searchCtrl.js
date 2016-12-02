38c5f88a38138db521a38375de1f25f0fbf27699
mohitnavlakha <mohit-navlakha@uiowa.edu>
2016-12-02 16:32:18 -0600

    change in search ctrl

--- app/searchCtrl.js
+++ app/searchCtrl.js
@@ -7,6 +7,8 @@ app.controller("searchCtrl", function(md5, $http, $scope){
     $scope.searchManagerResults=[];
 
     $scope.searchClicked = function() {
+        $scope.searchVoterResults=[];
+        $scope.searchManagerResults=[];
         $http.post('server/searchUsers.php?user_type='+$scope.search.user_type+
             '&user_name='+$scope.search.user_name+
             '&first_name='+$scope.search.first_name+
